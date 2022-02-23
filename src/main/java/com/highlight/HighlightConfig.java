316a0305a20c602909123782413cb051daefffd4
bulletbrainsurgery <neggie333@gmail.com>
2022-02-23 21:43:37 +1100

    name

--- src/main/java/com/highlight/HighlightConfig.java
+++ src/main/java/com/highlight/HighlightConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.highlight;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface HighlightConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
