f5010006dedf1514f4f1bc181304b2066914cb20
mc <neggie333@gmail.com>
2024-02-23 16:07:22 +1100

    fixed config? removed redundant imports

--- src/main/java/com/ticktimestamp/TickTimestampConfig.java
+++ src/main/java/com/ticktimestamp/TickTimestampConfig.java
@@ -1,5 +1,6 @@
 package com.ticktimestamp;
 
+import java.awt.Color;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
@@ -8,12 +9,28 @@ import net.runelite.client.config.ConfigItem;
 public interface TickTimestampConfig extends Config
 {
 	@ConfigItem(
-		keyName = "deltaTick",
-		name = "Relative tick",
-		description = "Whether to display the tick timestamp as a tick count relative to the previous timestamped chat message"
+		keyName = "chatmessages",
+		name = "Chat messages",
+		description = "Display timestamp on public, private, fc, clan chats"
 	)
-	default boolean deltaTick()
+	default boolean chatMessage()
 	{
 		return false;
 	}
+
+	@ConfigItem(
+		keyName = "opaqueTimestamp",
+		name = "Timestamps (opaque)",
+		position = 1,
+		description = "Colour of Timestamps from the Timestamps plugin (opaque)"
+	)
+	Color opaqueTimestamp();
+
+	@ConfigItem(
+		keyName = "transparentTimestamp",
+		name = "Timestamps (transparent)",
+		position = 2,
+		description = "Colour of Timestamps from the Timestamps plugin (transparent)"
+	)
+	Color transparentTimestamp();
 }
--- src/main/java/com/ticktimestamp/TickTimestampPlugin.java
+++ src/main/java/com/ticktimestamp/TickTimestampPlugin.java
@@ -2,21 +2,30 @@ package com.ticktimestamp;
 
 import com.google.inject.Inject;
 import com.google.inject.Provides;
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.ChatMessage;
+import net.runelite.api.MessageNode;
+import net.runelite.api.ScriptID;
+import net.runelite.api.Varbits;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ScriptCallbackEvent;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.ColorUtil;
 
 @PluginDescriptor(
 	name = "Tick Timestamp",
-	description = "Display a game client tick count as a timestamp on game chat messages.<br>" +
-		"Useful for knowing if you are on pace when skilling or timing actions.",
-	tags = {"tick", "count", "timestamp"}
+	description = "Display a game client tick count as a timestamp on game chat messages",
+	tags = {"tick", "count", "timestamp"},
+	conflicts = "Chat Timestamps"
 )
 public class TickTimestampPlugin extends Plugin
 {
@@ -24,10 +33,14 @@ public class TickTimestampPlugin extends Plugin
 	private Client client;
 
 	@Inject
+	private ClientThread clientThread;
+
+	@Inject
 	private TickTimestampConfig config;
 
-	private int lastTickCount;
 	private int loginGameTick = 0;
+
+	private final Set<ChatMessageType> timestampedTypes = new HashSet<>(Arrays.asList(ChatMessageType.CONSOLE,ChatMessageType.SPAM,ChatMessageType.GAMEMESSAGE));
 
 	@Provides
 	TickTimestampConfig provideConfig(ConfigManager configManager)
@@ -35,32 +48,87 @@ public class TickTimestampPlugin extends Plugin
 		return configManager.getConfig(TickTimestampConfig.class);
 	}
 
-	@Subscribe(priority = -100)
-	public void onChatMessage(ChatMessage event)
-	{
-		ChatMessageType type = event.getType();
-		if (!ChatMessageType.GAMEMESSAGE.equals(type) && !ChatMessageType.SPAM.equals(type) && !ChatMessageType.CONSOLE.equals(type))
-		{
-			return;
-		}
-
-		int timestamp = config.deltaTick() ? (client.getTickCount() - lastTickCount) : (client.getTickCount() - loginGameTick + 1);
-		lastTickCount = client.getTickCount() + 1;
-
-		event.getMessageNode().setValue(timestamp + ": " + event.getMessageNode().getValue());
-	}
-
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged event)
-	{
-		GameState state = event.getGameState();
-
-		switch (state)
+	private void onGameStateChanged(GameStateChanged e){
+		switch(e.getGameState())
 		{
 			case LOGGING_IN:
 			case HOPPING:
 				loginGameTick = client.getTickCount();
 				break;
+		}
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (event.getGroup().equals("ticktimestamp"))
+		{
+			switch (event.getKey())
+			{
+				case "opaqueTimestamp":
+				case "transparentTimestamp":
+					clientThread.invokeLater(() -> client.runScript(ScriptID.SPLITPM_CHANGED));
+					break;
+				case "chatmessages":
+					updateTypes();
+			}
+		}
+	}
+
+	@Subscribe
+	private void onScriptCallbackEvent(ScriptCallbackEvent event)
+	{
+		if (!"chatMessageBuilding".equals(event.getEventName()))
+		{
+			return;
+		}
+
+		int uid = client.getIntStack()[client.getIntStackSize() - 1];
+		final MessageNode messageNode = client.getMessages().get(uid);
+		assert messageNode != null : "chat message build for unknown message";
+
+		// rebuilt per message. if just added, it will have epoch time: big number
+		if (messageNode.getTimestamp() >> 24 > 1){
+			messageNode.setTimestamp(client.getTickCount());
+		}
+
+		String timestamp = "";
+
+		if (timestampedTypes.contains(messageNode.getType())){
+			timestamp = getTimestampString(messageNode);
+		}
+
+		client.getStringStack()[client.getStringStackSize() - 1] = timestamp;
+	}
+
+	private Color getTimestampColour()
+	{
+		boolean isChatboxTransparent = client.isResized() && client.getVarbitValue(Varbits.TRANSPARENT_CHATBOX) == 1;
+
+		return isChatboxTransparent ? config.transparentTimestamp() : config.opaqueTimestamp();
+	}
+
+	private String getTimestampString(MessageNode msg){
+		String timestamp = String.valueOf((msg.getTimestamp() - loginGameTick + 1));
+
+		Color timestampColour = getTimestampColour();
+		if (timestampColour != null)
+		{
+			timestamp = ColorUtil.wrapWithColorTag(timestamp, timestampColour);
+		}
+
+		return timestamp + ":";
+	}
+
+	private void updateTypes(){
+		if (config.chatMessage()){
+			timestampedTypes.addAll(Arrays.asList(ChatMessageType.PUBLICCHAT, ChatMessageType.PRIVATECHAT, ChatMessageType.PRIVATECHATOUT,
+				ChatMessageType.CLAN_CHAT, ChatMessageType.CLAN_GUEST_CHAT));
+		}
+		else {
+			Arrays.asList(ChatMessageType.PUBLICCHAT,ChatMessageType.PRIVATECHAT,ChatMessageType.PRIVATECHATOUT,
+				ChatMessageType.CLAN_CHAT,ChatMessageType.CLAN_GUEST_CHAT).forEach(timestampedTypes::remove);
 		}
 	}
 }
